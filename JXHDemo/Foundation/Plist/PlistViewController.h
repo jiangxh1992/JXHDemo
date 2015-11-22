289329264b43dea7e937d395443712e3cf89af03
albeeert <919575700@qq.com>
2015-11-22 18:00:51 +0800

    update

---
+++ JXHDemo/Foundation/Plist/PlistViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlistViewController.h
+//  JXHDemo
+//
+//  Created by 919575700@qq.com on 11/22/15.
+//  Copyright © 2015 Jiangxh. All rights reserved.
+//  plist文件的存取
+
+#import <UIKit/UIKit.h>
+
+@interface PlistViewController : UIViewController
+
+@end
---
+++ JXHDemo/Foundation/Plist/PlistViewController.m
@@ -0,0 +1,37 @@
+//
+//  PlistViewController.m
+//  JXHDemo
+//
+//  Created by 919575700@qq.com on 11/22/15.
+//  Copyright © 2015 Jiangxh. All rights reserved.
+//
+
+#import "PlistViewController.h"
+
+@interface PlistViewController ()
+
+@end
+
+@implementation PlistViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
