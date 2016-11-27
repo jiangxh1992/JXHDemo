4e28ebd124f2a7793bf9fe0f71afad4a9a72da9f
Xinhou Jiang <xinhoujiang@Xinhous-MacBook-Pro.local>
2016-11-27 19:28:35 +0800

    update;

--- JXHDemo/Foundation/Plist/PlistViewController.m
+++ JXHDemo/Foundation/Plist/PlistViewController.m
@@ -16,20 +16,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // 读取工程中的plist文件，这里设置的工程的plist是一个字典，也可以用数组plist
+    // 1.读取工程中的plist文件，这里设置的工程的plist是一个Dictionary字典，也可以用Array数组plist
     NSMutableDictionary *data = [[NSMutableDictionary alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"test" ofType:@"plist"]];
     NSLog(@"从工程plist读取的数据:%@",data);
     
     // 添加新数据到字典对象中
     [data setObject:@"4444" forKey:@"d"];
-    NSLog(@"写入沙盒的数据:%@",data);
+    NSLog(@"将要写入沙盒的数据:%@",data);
     
     // 获取沙盒路径,这里"/demo.plist"是指新建的沙盒里plist文件路径，一定要加“/”!!!
     NSString *filePath = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask,YES) lastObject] stringByAppendingPathComponent:@"/demo.plist"];
-    // 将工程中的数据新字典写入沙盒
+    // 2.将更新了的工程中的数据写入沙盒
     [data writeToFile:filePath atomically:YES];
     
-    // 读取沙盒数据  
+    // 3.读取沙盒数据  
     NSMutableDictionary *dic = [[NSMutableDictionary alloc] initWithContentsOfFile:filePath];
     NSLog(@"从沙盒读取的数据:%@",dic);
     // ...
--- README.md
+++
@@ -1,3 +0,0 @@
-# JXHDemo
-
-![这里写图片描述](http://img.blog.csdn.net/20161127191124562)
